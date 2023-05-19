77208c8a34ad2b4cd8ccebe4d2fcb64002bb7a0d
yochaiankava <meshek.ankava@gmail.com>
2023-05-19 12:35:38 +0300

    added color input, session & logout link

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"asdfsafsg346345F4Q8z\n\xec]/'
 
 students = [
     {"name": "John Doe", "phone": "1234567890"},
@@ -8,31 +9,65 @@ students = [
     {"name": "Michael Johnson", "phone": "5555555555"}
 ]
 
+
 @app.route("/", methods=['GET', 'POST'])
 def login():
+    message = ""
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
-        
+        color = request.form.get("color")
+        print(f"user:{username}, password:{password}. color:{color}")
         if username == "yochai" and password == "123":
+            session["logged_in_user"] = username
+            session["color"] = color
             return redirect("/students")
         else:
-            error_message = "LOGIN ERROR. Please try again."
-            return render_template('login.html', error_message=error_message)
-    
-    return render_template('login.html')
+            message = "Error in login"              
+    return render_template('login.html', error_message=message)
 
 @app.route("/students", methods=['GET', 'POST'])
 def view_students():
-    if request.method == 'POST':
-        search_name = request.form.get("search_name")
-        results = [student for student in students if search_name.lower() in student["name"].lower()]
-        return render_template('students.html', students=results, search_name=search_name)
+    # check if logged in. if not redirect("/")
+    if not session.get("logged_in_user"):
+        return redirect("/")
+    return render_template("students.html", students = students, logged_in_user= session.get("logged_in_user"))
+    # if request.method == 'POST':
+    #     search_name = request.form.get("search_name")
+    #     print({search_name})
+    #     results = [student for student in students if search_name.lower() in student["name"].lower()]
+    #     return render_template('students.html', students=results, search_name=search_name)
     
-    return render_template('students.html', students=students)
+    # return render_template('students.html', students=students)
+@app.route("/search")
+def search():
+    search = request.args.get("search")
+    phone = request.args.get("phone")
+    color = request.args.get("color")
+    new_list = []
+    print(f"search-{search} phone-{phone} color-{color}")
+    
+    for student in students:
+        print(f"student-{student}")        
+        if search in student["name"] and phone in student["phone"]:
+            new_list.append(student)
+    # new_list = [student for student in students(dic) if search in student]   
+    print(f"new_list-{new_list}")
+    return render_template("students.html", students = new_list, logged_in_user=session.get("logged_in_user"), color=session.get("color"))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    #session["logged_in_user"] = ""
+    return redirect("/")
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    # Custom logic for handling 404 errors
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-# check commit
+# check
